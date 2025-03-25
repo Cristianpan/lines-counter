2f7e0dd7d16898f15f651e29ccfbdf5ff75f7e20
cristianpan <panzaldivarcristian@gmail.com>
2025-03-25 09:57:07 -0600

    refactor: ♻️ Refactor FileCounter for improved readability

--- src/main/java/com/proy/files/FileCounter.java
+++ src/main/java/com/proy/files/FileCounter.java
@@ -1,64 +1,88 @@
 package com.proy.files;
+
 import java.io.File;
 import java.util.List;
 
+import com.proy.exceptions.CodeStandarException;
 import com.proy.model.CodeSegment;
 import com.proy.readers.ReaderFile;
 import com.proy.validator.validatorContext.CodeValidationContext;
 import com.proy.validator.validatorControlers.StructureCountValidator;
 
 /**
- * La clase "FileCounter" proporciona los métodos que se necesitan para empezar el conteo de lineas y validación de un archivo
+ * La clase "FileCounter" proporciona los métodos que se necesitan para empezar
+ * el conteo de lineas y validación de un archivo
  * 
- * @version 1.0
+ * @version 1.1
  */
 
 public class FileCounter {
 
     private File file;
-    private CodeValidationContext codeValidationContext;
     private CodeSegment codeSegment;
 
     public FileCounter(File file) {
-       this.file = file;
-       this.codeSegment = new CodeSegment();
+        this.file = file;
+        this.codeSegment = new CodeSegment();
     }
 
-    /*
-     * Llama al CodeValidationContext para el conteo de líneas físicas y lógicas siempre y cuando sea válido el formato
-     * Instanciando el primer validador de FileStructureValidator que solo permite estructuras de organización y de estructuras de definición
+    /**
+     * Llama al CodeValidationContext para el conteo de líneas físicas y lógicas
+     * siempre y cuando sea válido el formato
+     * Instanciando el primer validador de FileStructureValidator que solo permite
+     * estructuras de organización y de estructuras de definición
      */
     public void countLinesInFile() {
-        ReaderFile readerFile = new ReaderFile();
-        List<String> lines = readerFile.readFileLines(file);
-        int numLines = lines.size();
-        if (FileCounter.isJavaFile(file)) {
-            try {
-                codeValidationContext = new CodeValidationContext();
-                codeValidationContext.setStandardValidator(new StructureCountValidator(codeValidationContext));
-                codeValidationContext.validate(lines);
-                this.codeSegment = new CodeSegment(codeValidationContext.getPhysicalLines(), codeValidationContext.getLogicalLines());
-                this.codeSegment.setTitle(file.getName());
-            } catch (Exception e) {
-                int errorLine = numLines-lines.size()+1;
-                System.out.println(this.file.getName());
-                System.err.println(e.getMessage()+ "Línea. "+errorLine);
-            }
-        } else{
+        if (!FileCounter.isJavaFile(file)) {
             System.out.println(this.file.getName() + " no es un archivo con extensión válida");
+            return;
+        }
+
+        List<String> lines = new ReaderFile().readFileLines(file);
+        int initialLines = lines.size();
+
+        try {
+            validateAndSetCodeSegment(lines);
+        } catch (CodeStandarException e) {
+            int errorLine = initialLines - lines.size() + 1;
+            System.out.println(file.getName());
+            System.err.println(e.getMessage() + " Línea: " + errorLine);
+        } catch (Exception e) {
         }
     }
 
     /**
-     * Verifica si el archivo es un archivo Java (.java)
+     * Genera el contexto para la validación de las líneas del archivo y lo valida 
+     * 
+     * @param lines las líneas del archivo
+     * @throws CodeStandarException si el archivo no cumple con el estándar
+     */
+    private void validateAndSetCodeSegment(List<String> lines) throws CodeStandarException {
+        CodeValidationContext codeValidationContext = new CodeValidationContext();
+        codeValidationContext.setStandardValidator(new StructureCountValidator(codeValidationContext));
+        codeValidationContext.validate(lines);
+
+        this.codeSegment = codeValidationContext.getCodeSegment();
+        this.codeSegment.setTitle(file.getName());
+    }
+
+    /**
+     * Verifica si un archivo es un archivo Java (.java)
+     * 
      * @param file el archivo a verificar
-     * @return {@code true} si el archivo es un archivo Java, {@code false} en caso contrario
+     * @return {@code true} si el archivo es un archivo Java, {@code false} en caso
+     *         contrario.
      */
     public static boolean isJavaFile(File file) {
         return file.isFile() && file.getName().endsWith(".java");
     }
-    
-    public CodeSegment getCodeSegment(){
+
+    /**
+     * Devuelve el segmento de código del archivo
+     * 
+     * @return el segmento de código del archivo
+     */
+    public CodeSegment getCodeSegment() {
         return this.codeSegment;
     }
 }
